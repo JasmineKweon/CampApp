030d7524a8de2a1c85849eb9430ac0f9476d1e2f
JasmineKweon <jasmine.j.kweon@gmail.com>
2022-01-12 16:22:22 +0900

    Added Update part

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //Express Routing
+//Need to npm i express on terminal to use
 const express = require('express');
 const app = express();
 
 //mongoose Connect
+//Need to npm i mongoose on terminal to use
 const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 async function main() {
@@ -10,6 +12,7 @@ async function main() {
 }
 
 //Define view engine and views
+//Need to npm i ejs on terminal to use
 const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +29,11 @@ const Campground = require('./models/campground');
 //In order to receive urlencoded as req.body
 app.use(express.urlencoded({ extended: true }));
 
+//In order to use GET/POST method as PUT/DELETE,etc methods
+//Need to npm i method-override on terminal to use
+const methodOverride = require('method-override');
+//_method should match with _method on ejs file, doesn't have to be _method
+app.use(methodOverride('_method'));
 
 //API calls
 app.get('/', (req, res) => {
@@ -35,7 +43,7 @@ app.get('/', (req, res) => {
     //res.send('msg') -> displays msg on page
 })
 
-//
+
 app.get('/campgrounds', async(req, res) => { // If you have await in function, you need to make the function async
     const campgrounds = await Campground.find({}); //Campground.find({}) takes time, therefore need "await"
     res.render('campgrounds/index', { campgrounds });
@@ -53,6 +61,19 @@ app.get('/campgrounds/:id', async(req, res) => {
 app.post('/campgrounds', async(req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+    //Redirect is needed to prevent user to refreshing the page and repeat updating
+})
+
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+    console.log(campground)
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
