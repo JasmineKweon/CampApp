c5caafdb860e79ebf052e3d5d57ed1d4dcb3c94d
JasmineKweon <jasmine.j.kweon@gmail.com>
2022-01-12 16:26:15 +0900

    Added DELETE

--- app.js
+++ app.js
@@ -77,6 +77,12 @@ app.put('/campgrounds/:id', async(req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
 //Listening on port 3000
 app.listen(3000, () => {
     console.log("Serving on port 3000");
