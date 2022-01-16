bee333bb52bf7dd7a483c17602a73ca4b394ec38
JasmineKweon <jasmine.j.kweon@gmail.com>
2022-01-16 21:20:50 +0900

    Delete feature edited
    when user delete campgrounds, its reviews all deleted

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,7 @@
 
 //Require mongoose to use it
 const mongoose = require('mongoose');
+const review = require('./review');
 
 //Create Schema
 const Schema = mongoose.Schema;
@@ -16,6 +17,16 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function(doc) {
+    if (doc) {
+        await review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 //Export just created schema, so it can be used in other files
